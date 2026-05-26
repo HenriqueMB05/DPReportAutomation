9103590f6c21b4be902de4278ad020cdcd1ea843
Matosh <matosh38@gmail.com>
2026-05-25 23:24:27 -0300

    fix(EmailLookUp): verificando se o email é uma solicitação de efetivo

--- parsing/EmailLookUp.py
+++ parsing/EmailLookUp.py
@@ -1,18 +1,32 @@
+from .ServiceLookUp import ServiceLookUp
+from email_reader.Email import Email
+
 class EmailLookUp:
     def __init__(self):
+        self.email_manager = Email()
         self.key_word = "efetivo"
-        self.list_email = validation_list(email)
+        self.service_list = {}
+        self.qualify_service = ServiceLookUp()
+        self.email_list = self._validation_list()
 
-    
-    def _validation_list(self, email: list):
-        if not email:
-            print("Não há email na caixa de entrada")
-            return
-        return email
-            
+    def _validation_list(self):
+        try:
+            self.email_manager.conectar()
+            emails = self.email_manager.catch_email()
+            if not emails:
+                print("Nenhum email na caixa de entrada!")
+                return[]
+            return emails
+        except Execption as err:
+            print(f"Erro ao abrir/buscar emails: {err}")
+            return []
 
     def mail_resolver(self):
-        mails = []
-        for k, v in self.list_email.items():
-            if self.key_word in k:
-                continue
+        for email in self.email_list:
+            subject = email["assunto"].lower().split("\n")
+            for mail in subject:
+                if self.key_word.lower() in mail:
+                    service_code = self.qualify_service.service_resolver(email['corpo']).copy()
+                    if service_code:
+                        self.service_list.update({email['id']: service_code})
+        return self.service_list
