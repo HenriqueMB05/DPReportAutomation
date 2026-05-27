f57223a7e3288dd899fc50ae70fcc8ee33bc4b7f
Matoshi_GD <matosh38@gmail.com>
2026-05-27 17:43:30 -0300

    fix(EmailLookUp): melhorando a lógica de mail resolver

--- parsing/EmailLookUp.py
+++ parsing/EmailLookUp.py
@@ -2,11 +2,11 @@ from .ServiceLookUp import ServiceLookUp
 from email_reader.Email import Email
 
 class EmailLookUp:
-    def __init__(self):
-        self.email_manager = Email()
+    def __init__(self, email_manager, servicelookup):
+        self.email_manager = email_manager
         self.key_word = "efetivo"
         self.service_list = {}
-        self.qualify_service = ServiceLookUp()
+        self.qualify_service = servicelookup
         self.email_list = self._validation_list()
 
     def _validation_list(self):
@@ -20,14 +20,12 @@ class EmailLookUp:
         except Exception as err:
             print(f"Erro ao abrir/buscar emails: {err}")
             return []
-
+    
     def mail_resolver(self):
-        for email in self.email_list:
-            subject = email["assunto"].lower().split("\n")
-            for mail in subject:
-                service_code = []
-                if self.key_word.lower() in mail:
-                    service_code = self.qualify_service.service_resolver(email['corpo']).copy()
-                    if service_code:
-                        self.service_list.update({email['id']: service_code})
+        service_mail = self.email_list
+        for email in service_mail:
+            if self.key_word in email["assunto"].strip().lower():
+                service_code = self.qualify_service.service_resolver(email['corpo']).copy()
+                if service_code:
+                    self.service_list.update({email['id']:service_code})
         return self.service_list
