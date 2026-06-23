1d492edfd2d04d6577144602eb74327eedc6510c
Matoshi_GD <matosh38@gmail.com>
2026-06-23 17:54:57 -0300

    fix: renomeando o pacote email_manager na main

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-    from email_reader import Email
-    from parsing import ServiceLookUp, EmailLookUp
+from email_manager import Email
+from parsing import ServiceLookUp, EmailLookUp
+qualify_email = EmailLookUp()
 
-    qualify_email = EmailLookUp()
-
-    service_list = qualify_email.mail_resolver()
-    for email_id, work_field in service_list.items():
-        print(f"{'-'*30}")
-        print(email_id)
-        for k, v in enumerate(work_field):
-            print(v)
+service_list = qualify_email.mail_resolver()
+for email_id, work_field in service_list.items():
+    print(f"{'-'*30}")
+    print(email_id)
+    for k, v in enumerate(work_field):
+        print(v)
 
 
 #if __name__ == "__main__":
-    #   services_teste = ServiceLookUp()
-    #  obras = services_teste.flat_services
+#   services_teste = ServiceLookUp()
+#  obras = services_teste.flat_services
 # print(obras)
