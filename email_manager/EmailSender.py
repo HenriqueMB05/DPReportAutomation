d735cc285c1bdd4ef424d0fa16796e0893830d2a
Matosh <matosh38@gmail.com>
2026-06-30 22:20:18 -0300

    Fix: montagem do email

--- email_manager/EmailSender.py
+++ email_manager/EmailSender.py
@@ -11,14 +11,17 @@ load_dotenv()
 class EmailSender:
     def __init__(self): 
         self.email = EmailMessage()
-        self.corpo = r"<p>Olá sior cookie pisca, nois que rraquia ceu emiu, pu favor nois da a senha."
+        self.corpo = self._validate_text(corpo)
+        self.subject = self._validate_subject(subject)
         self.user = os.getenv("EMAIL_USER")
         self.pswd = os.getenv("EMAIL_PSWD")
 
     def sent(self):
-        self.email['Subject'] = "Pru favo"
+        self.email['Subject'] = 
         self.email['From'] = self.user
-        self.email['To'] = os.getenv("EMAIL_FROM")
+        self.email['To'] = os.getenv("EMAIL_FROM") 
+        # Estou usando um email pre-setado para teste
+        # O correto é puxar o email do remetente
         self.email.set_content(self.corpo, subtype ='html')
         try:
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as servidor:
@@ -26,6 +29,8 @@ class EmailSender:
                 servidor.send_message(self.email)
         except Exception as err:
             print(f"Erro ao enviar email: {err}")
+    
+    def _validate_subject(self, subject):
 
     
 if __name__ == "__main__":
